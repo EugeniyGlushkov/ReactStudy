1ae75e59a089766366be2948588a38fbc03e462f
evgen <eugeniyglushkov@gmail.com>
2020-05-04 10:02:37 +0300

    React course * Create and config project

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/App.css';
+
+const App = () => {
+    return (
+        <div className="App">
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+            </ul>
+        </div>
+    );
+};
+
+export default App;
