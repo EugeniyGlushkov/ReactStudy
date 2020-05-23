08cd346250dcb05b9881211ccb09c4c3d2c71480
evgen <eugeniyglushkov@gmail.com>
2020-05-23 22:40:14 +0300

    26: Add dialogs items

---
+++ src/components/Dialogs/DialogItem/DialogItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from "../Dialogs.module.css";
+import {NavLink} from "react-router-dom";
+
+const DialogItem = (props) => {
+    let url = '/dialogs/' + props.id;
+    return (
+        <div className={s.dialog}>
+            <NavLink to={url} activeClassName={s.activeLink}>{props.name}</NavLink>
+        </div>
+    );
+};
+
+export default DialogItem;
