fd9bf075b1712ae6f071d554f20df4d2e59871cb
evgen <eugeniyglushkov@gmail.com>
2020-07-20 09:52:40 +0300

    51: Create request to a server config webpack file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -59,6 +59,14 @@ module.exports = {
                     'css-loader'
                 ],
                 exclude: /\.module\.css$/
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
             }
         ]
     },
