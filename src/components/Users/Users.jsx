9ef507c337e53bdda8583ab6adf69aa30a32f9bc
evgen <eugeniyglushkov@gmail.com>
2020-07-20 09:54:00 +0300

    51: Create request to a server config webpack file-loader

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,77 +1,14 @@
 import React from 'react';
 import styles from './users.module.css';
+import * as axios from 'axios';
+import userPhoto_default from '../../assets/images/user_default.png'
+
 
 const Users = (props) => {
 
     if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                photoUrl: 'https://citaty.info/files/characters/5039.jpg',
-                followed: true,
-                fullName: 'Dimych',
-                status: `I'm a boss'`,
-                location: {
-                    city: 'Minsk',
-                    country: 'Belarus'
-                }
-            },
-            {
-                id: 2,
-                photoUrl: 'https://citaty.info/files/characters/5039.jpg',
-                followed: false,
-                fullName: 'Andrew',
-                status: `I'm a boss to'`,
-                location: {
-                    city: 'Minsk',
-                    country: 'Belarus'
-                }
-            },
-            {
-                id: 3,
-                photoUrl: 'https://citaty.info/files/characters/5039.jpg',
-                followed: true,
-                fullName: 'Sveta',
-                status: `I'm pretty'`,
-                location: {
-                    city: 'Kiev',
-                    country: 'Ukraine'
-                }
-            },
-            {
-                id: 4,
-                photoUrl: 'https://citaty.info/files/characters/5039.jpg',
-                followed: false,
-                fullName: 'Sasha',
-                status: `Kitty'`,
-                location: {
-                    city: 'Moscow',
-                    country: 'Russia'
-                }
-            },
-            {
-                id: 5,
-                photoUrl: 'https://citaty.info/files/characters/5039.jpg',
-                followed: false,
-                fullName: 'Viktor',
-                status: `WTF'`,
-                location: {
-                    city: 'St Peterburg',
-                    country: 'Russia'
-                }
-            },
-            {
-                id: 6,
-                photoUrl: 'https://citaty.info/files/characters/5039.jpg',
-                followed: true,
-                fullName: 'Valera',
-                status: `In search of incredible'`,
-                location: {
-                    city: 'Sevastopol',
-                    country: 'Russia'
-                }
-            }
-        ]);
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => props.setUsers(response.data.items));
     }
 
     return (
@@ -81,7 +18,8 @@ const Users = (props) => {
                         <div key={user.id}>
                             <span>
                                 <div>
-                                    <img src={user.photoUrl} className={styles.userPhoto}/>
+                                    <img src={user.photos.small != null ? user.photos.small : userPhoto_default}
+                                         className={styles.userPhoto}/>
                                 </div>
                                 <div>
                                     {user.followed === true
@@ -92,7 +30,7 @@ const Users = (props) => {
                             <span>
                                 <span>
                                     <div>
-                                        {user.fullName}
+                                        {user.name}
                                     </div>
                                     <div>
                                         {user.status}
@@ -100,10 +38,10 @@ const Users = (props) => {
                                 </span>
                                 <span>
                                     <div>
-                                        {user.location.city}
+                                        {'user.location.city'}
                                     </div>
                                     <div>
-                                        {user.location.country}
+                                        {'user.location.country'}
                                     </div>
                                 </span>
                             </span>
